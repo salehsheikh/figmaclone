5e0d64055d15d0d6393ff48883b942c320b25bd2
saleh sheikh <salehsheikh786@gmail.com>
2024-04-13 23:51:34 +0500

    first

---
+++ figmac/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Work_Sans } from "next/font/google";
+import "./globals.css";
+import { Room } from "./Room";
+
+const workSans = Work_Sans({ subsets: ["latin"],
+variable:'--font-work-sans',
+weight:['400','600','700']
+ });
+
+export const metadata: Metadata = {
+  title: "Create Figma clone",
+  description: "Created by saleh",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${workSans.className}bg-primary-grey-200`}>
+        <Room>
+        {children}
+        </Room>
+        </body>
+    </html>
+  );
+}
---
+++ figmac/components/Live.tsx
@@ -0,0 +1,50 @@
+import React, { useCallback } from 'react';
+import LiveCursors from './cursor/LiveCursors';
+import { useMyPresence, useOthers } from '@/liveblocks.config';
+
+type Presence = {
+  cursor: {
+    x: number;
+    y: number;
+  } | null;
+  message: string | null;
+  // ... other properties
+};
+
+const Live = () => {
+  const others = useOthers();
+  const [{ cursor }, updateMyPresence] = useMyPresence() as any;
+
+  const handlePointerMove = useCallback((event: React.PointerEvent) => {
+    event.preventDefault();
+    const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+    const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+    updateMyPresence({ cursor: { x, y } });
+  }, []);
+
+  const handlePointerLeave = useCallback((event: React.PointerEvent) => {
+    event.preventDefault();
+    updateMyPresence({ cursor: null, message: null });
+  }, []);
+
+  const handlePointerDown = useCallback((event: React.PointerEvent) => {
+    event.preventDefault();
+    const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+    const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+    updateMyPresence({ cursor: { x, y } });
+  }, []);
+
+  return (
+    <div
+      onPointerMove={handlePointerMove}
+      onPointerLeave={handlePointerLeave}
+      onPointerDown={handlePointerDown}
+      className="h-[100vh] w-full flex justify-center items-center text-center border-2"
+    >
+      <h1 className="text-5xl">hi </h1>
+      <LiveCursors others={others} />
+    </div>
+  );
+};
+
+export default Live;
