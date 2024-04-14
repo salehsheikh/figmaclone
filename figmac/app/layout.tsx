5827f338a08fdc6a0c72066e348ac1a194f13773
saleh sheikh <salehsheikh786@gmail.com>
2024-04-15 04:28:30 +0500

    updated

--- figmac/app/layout.tsx
+++ figmac/app/layout.tsx
@@ -1,30 +1,31 @@
-import type { Metadata } from "next";
 import { Work_Sans } from "next/font/google";
+
 import "./globals.css";
+
 import { Room } from "./Room";
 
-const workSans = Work_Sans({ subsets: ["latin"],
-variable:'--font-work-sans',
-weight:['400','600','700']
- });
 
-export const metadata: Metadata = {
-  title: "Create Figma clone",
-  description: "Created by saleh",
+
+export const metadata = {
+  title: "Figma Clone",
+  description:
+    "A minimalist Figma clone using fabric.js and Liveblocks for realtime collaboration",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body className={`${workSans.className}bg-primary-grey-200`}>
-        <Room>
+const workSans = Work_Sans({
+  subsets: ["latin"],
+  variable: "--font-work-sans",
+  weight: ["400", "600", "700"],
+});
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => (
+  <html lang='en'>
+    <body className={`${workSans.className} bg-black`}>
+      <Room>
         {children}
-        </Room>
-        </body>
-    </html>
-  );
-}
+      </Room>
+    </body>
+  </html>
+);
+
+export default RootLayout;
--- figmac/components/cursor/Cursor.tsx
+++ figmac/components/cursor/Cursor.tsx
@@ -1,16 +1,24 @@
-import CursorSVG from "@/public copy/assets/CursorSVG";
+import CursorSVG from "@/public/assets/CursorSVG";
+
 
 type Props={
     color:string;
     x:number;
     y:number;
-    message:string;
+    message?:string;
 }
 
 const Cursor = ({color,x,y,message}:Props) => {
   return (
     <div className="pointer-events-none absolute top-0 left-0"style={{transform:`translateX(${x}px) translateY(${y}px)`}}>
       <CursorSVG color={color}/>
+      {message &&(
+        <div className="absolute left-2 top-5 rounded-3xl px-4 py-2 "
+        style={{backgroundColor:color}}>
+
+          <p className="text-white whitespace-nowrap text-sm leading-relaxed">{message}</p>
+        </div>
+      )}
     </div>
   )
 }
